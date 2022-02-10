5768137c85fd09d3acdfcc98a19fa688d8076939
quynh1210 <quynh1210.nt@gmail.com>
2022-02-10 09:59:50 +0700

    add file

---
+++ src/com/codegym/TacoMeal.java
@@ -0,0 +1,24 @@
+package com.codegym;
+
+public class TacoMeal extends Meal {
+
+    @Override
+    public void prepareIngredients() {
+        System.out.println("Getting ground beef and shells");
+    }
+
+    @Override
+    public void cook() {
+        System.out.println("Cooking ground beef in pan");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Mmm, Taco is tasty");
+    }
+
+    @Override
+    public void cleanUp() {
+        System.out.println("Doing the dishes");
+    }
+}
---
+++ src/com/codegym/TmeplateMethodDemo.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class TmeplateMethodDemo {
+    public static void main(String[] args) {
+
+        Meal meal1 = new HamburgerMeal();
+        meal1.doMeal();
+
+        System.out.println();
+
+        Meal meal2 = new TacoMeal();
+        meal2.doMeal();
+
+    }
+}
